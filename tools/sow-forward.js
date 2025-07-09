e729bd58e1c0e0427f88789246ddc99707c32b2f
aislum-studio <salum.rashid.znz@gmail.com>
2025-07-09 22:38:26 +0300

    Update sow-forward.js

--- tools/sow-forward.js
+++ tools/sow-forward.js
@@ -1,1 +1,37 @@
+// tools/sow-forward.js
 
+module.exports = {
+  name: "sowForward",
+  description: "Simulates forward sowing from a given pit index",
+  inputSchema: {
+    type: "object",
+    properties: {
+      board: { type: "array", items: { type: "number" } },
+      pitIndex: { type: "number" }
+    },
+    required: ["board", "pitIndex"]
+  },
+  outputSchema: {
+    type: "object",
+    properties: {
+      newBoard: { type: "array", items: { type: "number" } },
+      lastPit: { type: "number" }
+    }
+  },
+  handler: async ({ board, pitIndex }) => {
+    let seeds = board[pitIndex];
+    board[pitIndex] = 0;
+    let index = pitIndex;
+
+    while (seeds > 0) {
+      index = (index + 1) % board.length;
+      board[index]++;
+      seeds--;
+    }
+
+    return {
+      newBoard: board,
+      lastPit: index
+    };
+  }
+};
